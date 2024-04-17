c120d75ddc4680ec50c7ea0378b1b7c2f8b740ea
Lars Walther <larswalther@Laptop-von-Lars.fritz.box>
2024-04-17 08:38:52 +0200

    setup.py neue Python-Version

--- setup.py
+++ setup.py
@@ -44,16 +44,15 @@ setup(
     package_dir={'': 'src'},
     include_package_data=True,
     zip_safe=False,
-    python_requires="==2.7",
+    python_requires=">=3.8",
     install_requires=[
         'setuptools',
         # -*- Extra requirements: -*-
         'z3c.jbot',
         'Products.GenericSetup>=1.8.2',
         'plone.api>=1.8.4',
-        'plone.restapi < 7.0.0a1',
+        'plone.restapi',
         'plone.app.dexterity',
-        'plone.app.referenceablebehavior',
         'plone.app.relationfield',
         'plone.app.lockingbehavior',
         'plone.schema',
