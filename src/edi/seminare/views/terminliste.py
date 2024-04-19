07ae413fb0361d631cd38288e36b81b84d53e998
Lars Walther <larswalther@Laptop-von-Lars.fritz.box>
2024-04-19 11:55:25 +0200

    Dokumentation der Attribute

--- src/edi/seminare/views/terminliste.py
+++ src/edi/seminare/views/terminliste.py
@@ -8,6 +8,27 @@ from plone import api
 class Terminliste(BrowserView):
 
     def __call__(self):
+        """
+        Es wird eine Liste "self.seminartermine" zurückgegeben
+        Jedes Element der Terminliste hat folgende Schlüssel oder Attribute:
+        
+        start - Startdatum (nur für Sortierung)
+        end - Enddatum (nur für Sortierung)
+        zeit - formatierte Darstellung Datum, Uhrzeit
+        ort - Veranstaltungsort
+        title - Titel des Seminars (Thema)
+        url - Link in die Einzelansicht des Seminars
+        places - Anzeige der freien Plätze
+
+        self.seminartermine = [
+          'start':'Startdatum (nur für Sortierung)'
+          'end':'Enddatum (nur für Sortierung)',
+          'zeit':'formatierte Darstellung Datum, Uhrzeit',
+          'title':'Titel des Seminars (Thema)',
+          'url':'Link in die Einzelansicht des Seminars',
+          'places':'Anzeige der freien Plätze'
+        ]
+        """
         seminare = api.content.find(context=self.context, portal_type="Seminarangebot")
         formatted_termine = []
         for seminar in seminare:
