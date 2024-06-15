2668d6ad4d4f8a38569bab606771a0dfe9a7167a
Lars Walther <lwalther@novareto.de>
2024-06-15 18:47:11 +0200

    Back to development: 1.1

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='edi.seminare',
-    version='1.0',
+    version='1.1.dev0',
     description="Add-On for internal education events or courses",
     long_description=long_description,
     # Get more from https://pypi.org/classifiers/
