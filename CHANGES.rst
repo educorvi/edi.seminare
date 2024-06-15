15b6b02de6ddefcffd77a209417aa4c834166945
Lars Walther <lwalther@novareto.de>
2024-06-15 18:46:57 +0200

    Preparing release 1.0

--- CHANGES.rst
+++ CHANGES.rst
@@ -2,8 +2,8 @@ Changelog
 =========
 
 
-1.0a1 (unreleased)
-------------------
+1.0 (2024-06-15)
+----------------
 
 - Initial release.
   [meinkraeks@gmail.com]
--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='edi.seminare',
-    version='1.0a1',
+    version='1.0',
     description="Add-On for internal education events or courses",
     long_description=long_description,
     # Get more from https://pypi.org/classifiers/
